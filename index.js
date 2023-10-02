ac8edec358c3d285007d423d3d706e784cc27e37
Mohammed Ilyas Ahmed <47917247+ilyas122@users.noreply.github.com>
2023-10-02 13:07:28 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-//1. Use the inquirer npm package to get user input.
+
 
 import inquirer from 'inquirer';
 import qr from "qr-image";
@@ -34,8 +34,4 @@ inquirer
   }
 });
 
-//2. Use the qr-image npm package to turn the user entered URL into a QR code image.
 
-
-
-//3. Create a txt file to save the user input using the native fs node module.
